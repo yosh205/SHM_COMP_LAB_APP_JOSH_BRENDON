1da6773dbdb338e5639baf7391823e982105d0ec
Joshua <joshuaindig@gmail.com>
2024-10-14 14:22:41 -0400

    test newclass

---
+++ src/shm_comp_lab_app_josh_brendon/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package shm_comp_lab_app_josh_brendon;
+
+/**
+ *
+ * @author joshu
+ */
+public class NewClass {
+    
+}
