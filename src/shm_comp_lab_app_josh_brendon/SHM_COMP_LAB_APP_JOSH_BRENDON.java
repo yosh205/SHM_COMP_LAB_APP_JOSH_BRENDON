bc52379c9ba55b4f5d4458a1c06187f4a72ca8d3
McConne11 <bdmmcconnell@gmail.com>
2024-10-19 12:37:53 -0400

    Adding menu bar and file options to the menu bar

--- src/shm_comp_lab_app_josh_brendon/SHM_COMP_LAB_APP_JOSH_BRENDON.java
+++ src/shm_comp_lab_app_josh_brendon/SHM_COMP_LAB_APP_JOSH_BRENDON.java
@@ -4,10 +4,19 @@
  */
 package shm_comp_lab_app_josh_brendon;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -43,8 +52,50 @@ public class SHM_COMP_LAB_APP_JOSH_BRENDON extends Application{
         // Set up the scene
         //We should use gridpane to easily separate the different panes and menubar
         GridPane root = new GridPane();
-        root.add(view.getWavePane(),0,0);
-        root.add(setting.getControlPane(),0,1);
+        
+        BorderPane bp = new BorderPane();
+        
+        root.add(bp, 0, 0);
+        
+        MenuBar menuBar = new MenuBar();
+        
+        Menu fileMenu = new Menu("File");        
+        MenuItem newMenuItem = new MenuItem("New");
+        MenuItem exitMenuItem = new MenuItem("Exit");
+        
+        fileMenu.getItems().addAll(newMenuItem,
+                new SeparatorMenuItem(),
+                exitMenuItem
+        );   
+        
+        Menu wavesMenu = new Menu("Waves");
+        CheckMenuItem sinWaveMenuItem = new CheckMenuItem("Show Sine Wave");
+        sinWaveMenuItem.setSelected(true);  
+        CheckMenuItem cosWaveMenuItem = new CheckMenuItem("Show Cosine Wave");
+        cosWaveMenuItem.setSelected(true);
+        
+        wavesMenu.getItems().addAll(sinWaveMenuItem,
+                new SeparatorMenuItem(),
+                cosWaveMenuItem
+        );
+        
+        menuBar.getMenus().add(fileMenu);
+        menuBar.getMenus().add(wavesMenu);
+        
+        bp.setTop(menuBar);
+        
+        newMenuItem.setOnAction(actionEvent -> {
+            try {
+                this.start(stage);
+            } catch (Exception ex) {
+                Logger.getLogger(SHM_COMP_LAB_APP_JOSH_BRENDON.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+
+        exitMenuItem.setOnAction(actionEvent -> Platform.exit());
+        
+        root.add(view.getWavePane(),0,1);
+        root.add(setting.getControlPane(),0,2);
 
         Scene scene = new Scene(root, 600, 500);
         stage.setTitle("SHM");
